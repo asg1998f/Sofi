5bbc24128b0f423769ea91949f3c378e6ed79109
xavi <antunez.xavi.grao@gmail.com>
2025-02-06 21:49:29 +0100

    Cambios Xavi

--- src/components/Xavi/Xavi.jsx
+++ src/components/Xavi/Xavi.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import './Xavi.scss'
 
 const Xavi = () => {
   return (
-    <div>Xavi</div>
+    <>
+    <div id='bckg'>
+      <div id='container'>
+        <p>Echando la vista atrás, estos cuatro meses han pasado en un suspiro. A su vez, me da la sensación de que soy una persona muy diferente.
+          No voy a mentir, no tenía muy buena relación con la programación y dudaba un poco de querer dedicarme a esto el resto de mi vida,
+          pero ha sido gracias a tí que me he dado cuenta de que me gusta mucho este mundo y lo esencial que es tener un buen referente al aprender porque,
+          no solo eres una persona genial, también eres una profesora ejemplar, pero eso ya te lo habrán dicho mucho.
+        </p>
+        <p>
+          No sé dónde me llevará este bootcamp, pero me llevaré una parte de tí y la usaré cada vez que quiera escribir un código que me haga querer matarme.
+          Espero que también te quedes con algo nuestro, a ser posible que no sea el alcoholismo, la violencia, la lujuria ni la dislexia, 
+          aunque la alegría y la empatía ya la tenías antes de conocernos. Si en algún momento no estás bien, acuérdate de nosotros,
+          que nosotros nos estaremos acordando de tí seguro cada vez que hagamos un console.log. Tienes que aceptar que, aunque nuestros caminos se separen,
+          siempre puedes llamarme por si te has quedado soltera. El primer paso es admitirlo.
+        </p>
+        <p>
+          Termino mi parte diciendo que es fascinante lo mucho que puede aprender una persona pese a no poder leer tus diapositivas, ese es el nivel docente que tienes.
+        </p>
+        <p>
+          Te deseo lo mejor, ahora, y siempre.
+        </p>
+      </div>
+    </div>
+      <footer id='footer'>
+        <p>Sofía Petarda</p>
+        <p>Sal conmigo :)</p>
+      </footer>
+    </>
   )
 }
 
