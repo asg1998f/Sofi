36cb38ce608450261a11b42c015e07c5fba301f6
unknown <joan_mart_her@hotmail.com>
2025-02-07 09:47:09 +0100

    Add a button

--- src/components/Joan/Joan.jsx
+++ src/components/Joan/Joan.jsx
@@ -5,13 +5,13 @@ const Joan = () => {
   return (
     <div className='fondo'>
       <div className='parrafos'>
-        <p>Hola Sofi, qué tal? (Sé que no puedes contestarme, pero lo primero es la educación.)</p>
+        <p>Hola Sofi, qué tal? (Sé que no puedes contestarme, pero lo primero es la educación).</p>
         <p>Bueno, pues parece que esto ya se acaba... Es increíble lo rápido que pasa el tiempo(y más cuando te lo pasas bien) y la pena que da que todo esto se acabe ya y eso que ya llevo más de un mes haciéndome la idea de que esto se terminaba. Pero como me dijo un buen hombre en su momento, las cosas se tienen que acabar para que empiecen otras.</p>
         <p>Desde el primer día se te vio con mucha energía y empatía por todos nosotros y es algo que algunos de nosotros echamos en falta por parte de los profesores desde hace tiempo. Por eso algo que te quiero pedir por favor, es que sigas así y no cambies con los años (bueno si es a mejor sí jeje) porque como alumno, y creo que hablo en nombre de toda la clase, me ha gustado mucho aprender junto a ti, cada día me gustaba venir, cuando llegaban los viernes, me daba pena porque quería que volviera ser lunes para seguir aprendiendo en clase y sabía que si tenía cualquier duda, me ibas a ayudar a resolverla y podía preguntarte cualquier cosa, por muy tonta que fuera, sin miedo a que me miraras mal o me contestaras mal delante de la clase, como me pasaba en otros cursos.</p>
         <p>
         También quería darte las gracias por ser tan cercana con nosotros, porque en este corto periodo de tiempo, han surgido relaciones sanas entre todos los de clase y creo que tú has ayudado en eso. Aparte que igual podíamos preguntarte cosas de por qué no nos hacian efecto los estilos d nuestro archivo scss (no lo habíamos importado) que de nuestras vidas personales y eso creo que también ha ayudado a tener más confianza entre todos nosotros. Te estaremos siempre agradecidos por este increíble curso.
         </p>
-        <p>Y para ir terminando(disculpa el sermón), siento que de este curso he sacado colegas y amigos para mucho tiempo, tú entre ellos, ya sea para ir a los karts, a comer/cenar, a jugar a los bolos, a clases del coro o a tomar champú (si no es HyS no lo quiero), pero espero que esto no se acabe aquí. Ojalá que todo te siga yendo increíble y ya sabes que para cualquier cosa estamos aquí. ;)</p>
+        <p>Y para ir terminando(disculpa el sermón), siento que de este curso he sacado colegas y amigos para mucho tiempo, tú entre ellos, ya sea para ir a los karts, a comer/cenar, a jugar a los bolos, a clases del coro o a tomar champú (si no es HyS no lo quiero), pero espero que esto no se acabe aquí. Ojalá que todo te siga yendo increíble y ya sabes que para cualquier cosa estamos <a href="https://chat.whatsapp.com/JBnP2OxUOSELoal0hIBb7Y">aquí</a> ;)</p>
       </div>
     </div>
   )
